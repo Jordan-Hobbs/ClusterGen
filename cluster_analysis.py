e1f47ae8b77427592a2e71b5b2b7861e62ffe827
Jordan Hobbs <j.l.hobbs@leeds.ac.uk>
2025-05-27 20:27:58 +0100

    now reads from .out files

--- cluster_analysis.py
+++ cluster_analysis.py
@@ -1,27 +1,27 @@
 import os
 import re
 
-folder_path = r"C:\Users\Jordan\OneDrive - University of Leeds\Computational Data\ClusterGen\RM734\3AP_XTB2"
+folder_path = r"C:\Users\Jordan\OneDrive - University of Leeds\Computational Data\ClusterGen\RM734\3AP_XTB1XTB2"
 energy_values = []
 total_runtimes_sec = []
 
 for filename in os.listdir(folder_path):
-    if filename.endswith(".out"):
+    match = re.match(r"opt_cluster_\d+_stage" + re.escape("1") + r"\.out$", filename)
+    if match:
         file_path = os.path.join(folder_path, filename)
         with open(file_path, "r", encoding="utf-8", errors="ignore") as file:
             for line in file:
-                # TOTAL ENERGY line
                 match_energy = re.search(r"^\s*TOTAL ENERGY\s+([-+]?[0-9]*\.?[0-9]+)\s+Eh", line)
                 if match_energy:
                     energy_values.append(match_energy.group(1))
-                
-                # RUNTIME line
+
                 match_runtime = re.search(
                     r"CREST runtime \(total\)\s+(\d+) d,\s+(\d+) h,\s+(\d+) min,\s+([0-9.]+) sec", line)
                 if match_runtime:
                     d, h, m, s = map(float, match_runtime.groups())
                     total_seconds = d * 86400 + h * 3600 + m * 60 + s
                     total_runtimes_sec.append(total_seconds)
+
 
 # Save energies
 energy_output = os.path.join(folder_path, "energy_values.txt")
