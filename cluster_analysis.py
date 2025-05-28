530cdc8b7c24ca014e0fddd05aa071a217544461
Jordan Hobbs <j.l.hobbs@leeds.ac.uk>
2025-05-28 14:55:54 +0100

    fixed some bits

--- cluster_analysis.py
+++ cluster_analysis.py
@@ -1,41 +1,14 @@
 import os
 import re
 
-<<<<<<< HEAD
-# Folder containing the .xyz files
-folder_path = r"C:\Users\phyjlho\OneDrive - University of Leeds\Computational Data\ClusterGen\RM734\2AP_XTB2"
-
-# This will store the extracted Etot values
-etot_values = []
-
-# Loop through all .xyz files in the folder
-=======
 folder_path = r"C:\Users\Jordan\OneDrive - University of Leeds\Computational Data\ClusterGen\RM734\3AP_XTB1XTB2"
 energy_values = []
 total_runtimes_sec = []
 
->>>>>>> e1f47ae8b77427592a2e71b5b2b7861e62ffe827
 for filename in os.listdir(folder_path):
     match = re.match(r"opt_cluster_\d+_stage" + re.escape("1") + r"\.out$", filename)
     if match:
         file_path = os.path.join(folder_path, filename)
-<<<<<<< HEAD
-        with open(file_path, "r") as file:
-            lines = file.readlines()
-            if len(lines) >= 2:
-                # Match 'Etot=' followed by a number (x)
-                match = re.search(r"Etot=\s*([-+]?[0-9]*\.?[0-9]+)", lines[1])
-                if match:
-                    etot_values.append(match.group(1))
-
-# Save the Etot values to a text file
-output_file = os.path.join(folder_path, "energy_values.txt")
-with open(output_file, 'w') as out_file:
-    for value in etot_values:
-        out_file.write(value + '\n')
-
-print(f"Saved {len(etot_values)} Etot values to {output_file}")
-=======
         with open(file_path, "r", encoding="utf-8", errors="ignore") as file:
             for line in file:
                 match_energy = re.search(r"^\s*TOTAL ENERGY\s+([-+]?[0-9]*\.?[0-9]+)\s+Eh", line)
@@ -71,4 +44,3 @@ if total_runtimes_sec:
     print(f"Saved average runtime to {runtime_output}")
 
 print(f"Saved {len(energy_values)} TOTAL ENERGY values to {energy_output}")
->>>>>>> e1f47ae8b77427592a2e71b5b2b7861e62ffe827
